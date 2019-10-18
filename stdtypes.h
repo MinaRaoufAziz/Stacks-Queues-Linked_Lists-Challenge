f2a986f181ec05394ab9f0c94dda1697200ca7f4
MinaRaoufAziz <55676799+MinaRaoufAziz@users.noreply.github.com>
2019-10-18 13:52:46 +0200

    Create stdtypes.h
    first stack commit without testing

---
+++ stdtypes.h
@@ -0,0 +1,24 @@
+#ifndef STDTYPES_H
+#define STDTYPES_H
+typedef unsigned char uint8;
+typedef signed char sint8;
+typedef unsigned short int uint16;
+typedef signed short int sint16;
+typedef unsigned int uint32;
+typedef signed int sint32;
+typedef unsigned int uint64;
+typedef signed int sint64;
+typedef float float32;
+typedef double float64;
+typedef long double float128;
+typedef unsigned char* pu8;
+typedef signed char* ps8;
+typedef unsigned short int* pu16;
+typedef signed short int* ps16;
+typedef unsigned long int* pu32;
+typedef signed long int* ps32;
+typedef unsigned long long int* pu64;
+typedef signed long long int* ps64;
+typedef float* pf32;
+typedef double* pf64;
+#endif
